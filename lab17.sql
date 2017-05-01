eefd1f44bb8ab402086e5e49836d2555e01d1054
marinib <marin1704@gmail.com>
2017-05-01 09:58:59 -0400

    updated and fixed some commands

--- lab17.sql
+++ lab17.sql
@@ -3,9 +3,9 @@
 #SELECT * FROM customers WHERE CustomerID like "BL%"
 #SELECT * FROM orders LIMIT 100 
 #SELECT * FROM customers WHERE postalcode IN (1010, 3012, 12209, 05023)
-#SELECT * FROM orders WHERE shipregion <> null
+#SELECT * FROM orders WHERE shipregion IS NOT NULL
 #SELECT * FROM customers ORDER BY country, city
-#INSERT INTO customers (CustomerID, Contactname, City, Country, CompanyName) Values (100,"Marin B","Canton","USA","GC")
+#INSERT INTO customers (CustomerID, Contactname, City, Country, CompanyName) Values (100, "Marin B","Canton","USA","GC")
 #UPDATE orders SET shipregion = "Eurozone" WHERE shipcountry = "france"
 #DELETE FROM `order details` WHERE quantity = 1
 #SELECT AVG(quantity), MAX(quantity), MIN(quantity) FROM `order details` 
@@ -15,4 +15,4 @@
 #SELECT * FROM orders LEFT JOIN customers
 #SELECT * FROM orders RIGHT JOIN customers
 #SELECT *FROM employees WHERE ReportsTo IS NULL
-#SELECT *FROM employees WHERE ReportsTo IS NOT NULL (or > 0)
+#SELECT *FROM employees WHERE ReportsTo IS NOT NULL #(or > 0)
